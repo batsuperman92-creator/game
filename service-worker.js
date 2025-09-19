e078daf1dd2687b55371a1589ad143fdae74ae41
batsuperman92-creator <batsuperman92@gmail.com>
2025-09-19 09:50:44 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+﻿const CACHE_NAME = "capital-game-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// 설치 단계에서 캐시 저장
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  console.log("Service Worker: Installed and cached files");
+});
+
+// 활성화 단계 (오래된 캐시 제거)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  console.log("Service Worker: Activated");
+});
+
+// 요청 가로채서 캐시에서 불러오기
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
